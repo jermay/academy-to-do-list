52060caf151473909176f99b5bdd3b9e02fb7f0d
Jeremy May <may.jeremy.r@gmail.com>
2020-08-02 14:20:10 -0700

    in progress list

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const inputToDo = $('#inputToDo');
 const btnAdd = $('#btnAdd');
 const toDoContainer = $('#todo');
+const inprogressContainer = $('#inprogress');
+
+let lastId = 0;
+let toDosList = [];
+let inProgressList = [];
+
+
+btnAdd.click(onAddClick);
+
+function onAddClick(event) {
+    console.log('add clicked');
+    const value = inputToDo.val();
+    addToDoItem(value);
+}
 
 // add a to item
 function addToDoItem(message) {
-    item = '<li>' + message + '</li>';
-    toDoContainer.append(item);
+    let newItem = {
+        id: lastId,
+        message: message
+    };
+    toDosList.push(newItem);
+    lastId++;
+
+    addLi(newItem, toDoContainer, 'Start Task');
+    
+    console.log(toDosList);
+}
+
+function addLi(newItem, container, buttonText) {
+    const liItem = '<li id="' + newItem.id + '">' + newItem.message +
+        '<button class="btn btn-info ml-2" onclick="startTask(' + newItem.id + ')">' + buttonText + '</button>'
+        + '</li>';
+    container.append(liItem);
+}
+
+// start the task
+function startTask(id) {
+    console.log('start stask clicked');
+    console.log(id);
+
+    $('#' + id).remove();
+
+    let toDoItem = toDosList.find(
+        function (item) { return item.id === id });
+    
+    addLi(toDoItem, inprogressContainer, 'Done');
+
+    inProgressList.push(toDoItem);
+
+    console.log('in progess', inProgressList);
 }
